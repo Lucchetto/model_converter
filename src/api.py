f5ab4e39f8ea7b8d2484db301ad5726348d3f085
Lucchetto <lucchetto.tie@live.com>
2023-12-25 22:48:43 +0100

    Create api error enum
    Signed-off-by: Lucchetto <lucchetto.tie@live.com>

--- src/api.py
+++ src/api.py
@@ -1,11 +1,25 @@
+from enum import Enum
 import logging
-from flask import Flask, jsonify, request, send_file
+from flask import Flask, Response, jsonify, request, send_file
 import os
 import uuid
 
 from src.licensing import setup_pub_key, validate_license
 
-from .converter import convert_pth_to_onnx
+from .converter import UnsupportedModelArch, convert_pth_to_onnx
+
+class ApiErrorReason(Enum):
+    UNSUPPORTED_ARCH = "UNSUPPORTED_ARCH"
+    INVALID_LICENSE = 'INVALID_LICENSE'
+    UNSUPPORTED_FORMAT = 'UNSUPPORTED_FORMAT'
+    UNKNOWN = 'UNKNOWN'
+
+def api_error(reason: ApiErrorReason):
+    if reason == ApiErrorReason.INVALID_LICENSE:
+        status_code = 401
+    else:
+        status_code = 400
+    return jsonify({"reason": reason.value}), status_code
 
 def create_app():
     logging.basicConfig(level=logging.NOTSET)
@@ -19,17 +33,17 @@ def create_app():
     @app.errorhandler(ValueError)
     def handle_convert_error(error):
         if "is unsupported by chaiNNer. Please try another" in str(error):
-            reason = "UNSUPPORTED_FORMAT"
+            reason = ApiErrorReason.UNSUPPORTED_FORMAT
         else:
-            reason = "UNKNOWN"
+            reason = ApiErrorReason.UNKNOWN
 
-        return jsonify({"reason": reason}), 400
+        return api_error(reason)
 
     @app.route("/pthToOnnx", methods=['POST'])
     def pthToOnnx():
         if pub_key is not None:
             if not validate_license(pub_key, request.form.get("responseData"), request.form.get("signature")):
-                return jsonify({"reason": "INVALID_LICENSE"}), 401
+                return api_error(ApiErrorReason.INVALID_LICENSE)
 
         input_file = request.files['file']
         tmp_input_dir = os.path.join("tmp", "input_models")
@@ -50,6 +64,8 @@ def create_app():
                 os.path.abspath(tmp_output_path),
                 as_attachment=True,
                 download_name=os.path.splitext(input_file.filename)[0] + ".onnx")
+        except UnsupportedModelArch as e:
+            return api_error(ApiErrorReason.UNSUPPORTED_ARCH)
         finally:
             if os.path.isfile(tmp_input_path):
                 os.remove(tmp_input_path)
